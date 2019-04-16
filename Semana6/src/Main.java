b82cb6d9d329afb91f0ab1c4e1d457f4933f5870
Paula Galindo <paulagalindovf@live.com>
2019-04-16 19:33:10 -0300

    Add files via upload

---
+++ Semana6/src/Main.java
@@ -0,0 +1,42 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Mamifero m = new Mamifero();
+		Reptil r = new Reptil();
+		Peixe p = new Peixe();
+		Ave a = new Ave();
+		
+		m.setPeso(85.3f);
+		m.setIdade(2);
+		m.setMembros(4);
+		m.locomover();
+		m.alimentar();
+		m.emitirSom();
+		
+		p.setPeso(0.35f);
+		p.setIdade(1);
+		p.setMembros(0);
+		p.locomover();
+		p.alimentar();
+		p.emitirSom();
+		p.soltarBolha();	
+		
+		a.setPeso(0.89f);
+		a.setIdade(2);
+		a.setMembros(2);
+		a.locomover();
+		a.alimentar();
+		a.emitirSom();
+		a.fazerNinho();
+		
+		r.setPeso(3.0f);
+		r.setIdade(2);
+		r.setMembros(0);
+		r.locomover();
+		r.alimentar();
+		r.emitirSom();
+
+	}
+
+}
---
+++ Semana6/src/Reptil.java
@@ -0,0 +1,31 @@
+
+public class Reptil extends Animal {
+
+	private String corEscama;
+	@Override
+	public void locomover() {
+		System.out.println("Rastejando");
+
+	}
+
+	@Override
+	public void alimentar() {
+		System.out.println("Comendo pequenos animais");
+
+	}
+
+	public String getCorEscama() {
+		return corEscama;
+	}
+
+	public void setCorEscama(String corEscama) {
+		this.corEscama = corEscama;
+	}
+
+	@Override
+	public void emitirSom() {
+		System.out.println("Som de reptil");
+
+	}
+
+}
